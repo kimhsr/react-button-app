34231b7947e97a9cfabedd1f4001b14d0f83b14f
kimhsr <kimhsr@naver.com>
2022-12-15 01:55:37 +0900

    테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
